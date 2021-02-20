1c76c506c3577d795447a0e62f3f830b7e53b57f
seenuCFL <seenuindia001@gmail.com>
2021-02-20 15:20:02 +0530

    card edit

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -22,7 +22,7 @@ export default function Product(props) {
         </Link>
         <div className="card-body">
           <Link to={`/product/${product._id}`}>
-            <h2>{product.name}</h2>
+            <h2 style="width:300px">{product.name}</h2>
           </Link>
           <Rating
             rating={product.rating}
